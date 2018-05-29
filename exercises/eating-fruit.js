4b198e18581acc7921cc4da9035166a4064392b5
A (from Sicily) <gulina.angelo@gmail.com>
2018-05-29 15:34:18 +0200

    Solutions

--- exercises/eating-fruit.js
+++ exercises/eating-fruit.js
@@ -4,7 +4,10 @@ const {
 } = require('../utils/helpers');
 
 const {
-    compose
+    compose,
+    converge,
+    partial,
+    spreadArgs,
 } = require('../utils/fx');
 
 const biteConstConditional = x => x > 5 ? 1.8 : 1.3;
@@ -18,6 +21,25 @@ let timeToEatSimple; // a function of number of berries per the time const
 let timeToEat; // a function of number of berries per the time const per the bite const based on the weight of the berry
 
 // Start implementation
+
+const prop = key => obj => obj[key];
+const sum = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const multiplyArray = spreadArgs(multiply);
+const perTimeConst = partial(multiply, timeConst);
+
+const count = prop('count');
+const type = prop('type');
+const weight = prop('weight');
+
+const biteConst = compose(biteConstConditional, weight);
+const biteConstPerWeight = converge(multiplyArray, [biteConst, count]);
+const perBiteConst = compose(perTimeConst, biteConstPerWeight);
+
+totalBerries = berries.map(count).reduce(sum);
+totalBerriesWeight = berries.map(weight).reduce(sum);
+timeToEatSimple = perTimeConst(totalBerries);
+timeToEat = berries.map(perBiteConst).reduce(sum);
 
 module.exports = {
     totalBerries,
--- exercises/type-of-fruit.js
+++ exercises/type-of-fruit.js
@@ -8,7 +8,9 @@ const {
 } = require('../utils/helpers');
 
 const {
-    compose
+    both,
+    compose,
+    equal,
 } = require('../utils/fx');
 
 const heavy = 28;
@@ -32,6 +34,44 @@ let onlyLightApples;
 let onlyLightGreenApples;
 
 // Start implementation
+
+const onlyRed = equal('red');
+const onlyGreen = equal('green');
+
+const prop = key => obj => obj[key];
+const is = first => second => first === second;
+const greater = first => second => second >= first;
+const lower = first => second => second <= first;
+
+const color = prop('color');
+const type = prop('type');
+const weight = prop('weight');
+
+const isLemon = compose(is('lemon'), type);
+const isApple = compose(is('apple'), type);
+const isHeavy = compose(greater(heavy), weight);
+const isLight = compose(lower(heavy), weight);
+
+const isGreen = compose(is('green'), color);
+const isRed = compose(is('red'), color);
+const isYellow = compose(is('yellow'), color);
+
+const isHeavyApple = both(isApple, isHeavy);
+const isLightApple = both(isApple, isLight);
+const isGreenLightApple = both(isApple, isLight, isGreen);
+const isHeavyRedApple = both(isApple, isHeavy, isRed);
+const isYellowLemon = both(isLemon, isYellow);
+
+onlyRedApples = applesList.filter(onlyRed);
+onlyGreenApples = applesList.filter(onlyGreen);
+onlyHeavyFruits = fruitsList.filter(isHeavy);
+onlyApples = fruitsList.filter(isApple);
+onlyLemons = fruitsList.filter(isLemon);
+onlyYellowLemons = fruitsList.filter(isYellowLemon);
+onlyHeavyApples = fruitsList.filter(isHeavyApple);
+onlyHeavyRedApples = fruitsList.filter(isHeavyRedApple);
+onlyLightApples = fruitsList.filter(isLightApple);
+onlyLightGreenApples = fruitsList.filter(isGreenLightApple);
 
 module.exports = {
     onlyGreenApples,
